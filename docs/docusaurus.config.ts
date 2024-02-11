f09150815a1d8bdbdfd89944f5d9796ed872df24
kis0421 <kis0421@gmail.com>
2024-02-11 17:34:26 +0900

    Fix docusaurus config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'gwyng-dev', // Usually your GitHub org/user name.
+  projectName: 'react-form-mozard', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+            'https://github.com/gwyng-dev/react-form-mozard/tree/main/docs',
+        },        
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gwyng-dev/react-form-mozard/tree/main/blog',
         },
         theme: {
           customCss: './src/css/custom.css',
